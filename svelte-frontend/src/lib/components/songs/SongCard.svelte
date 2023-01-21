f17b4aa5500bbd0cbd4d20fc2139f2b8bc1373c8
Christopher <hilgi45@gmail.com>
2023-01-21 09:36:00 +0100

    Song overview with song card

---
+++ svelte-frontend/src/lib/components/songs/SongCard.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+
+    import { onMount } from "svelte";
+    import * as SongAPI from "../../api/songAPI";
+
+    export let songData;
+    export let artistData;
+    let coverImageURL;
+
+    onMount(async () => {
+        coverImageURL = SongAPI.getImageURL(songData);
+    })
+
+</script>
+
+<div class="bg-white dark:bg-neutral-900 max-w-xs">
+    <div class="relative">
+        <img class="z-30 aspect-square object-contain w-full bg-neutral-300" src="{coverImageURL}" alt=""/>
+        <div class="z-40 absolute bottom-0 right-0 mb-3 mr-6">
+            <button type="button" class="py-3 px-3 w-12 h-12 text-md inline-block rounded-full bg-red-600 text-white font-medium leading-tight uppercase shadow-md hover:bg-neutral-700 hover:shadow-lg active:bg-gray-700 active:shadow-lg transition duration-150 ease-in-out">
+                <i class="fa-solid fa-play"></i>
+            </button>
+        </div>
+    </div>
+    <div class="p-6">
+        <h5 class="text-white text-xl font-medium">
+            {songData.name} 
+        </h5>
+        <h2 class="text-gray-300 text-sm font-medium mb-2">
+            {artistData.name}
+        </h2>
+        <div class="flex flex-row">
+            <div class="mr-1 flex-1">
+                <button type="button" class="py-2 inline-block w-full bg-neutral-700 text-white font-medium text-xs leading-tight uppercase shadow-md hover:bg-sky-400 hover:shadow-lg active:bg-sky-700 active:shadow-lg transition duration-150 ease-in-out">
+                    Open
+                </button>
+            </div>
+            <div class="ml-1 flex-1">
+                <button type="button" class="py-2 inline-block w-full bg-neutral-700 text-white font-medium text-xs leading-tight uppercase shadow-md hover:bg-sky-400 hover:shadow-lg active:bg-sky-700 active:shadow-lg transition duration-150 ease-in-out">
+                    Edit
+                </button>
+            </div>
+            <div class="flex-none ml-2">
+                {#if songData.musicVideoYouTube !== ""}
+                    <a href="{songData.musicVideoYouTube}" target="_blank" rel="noreferrer" class="py-2 px-3 inline-block w-full bg-neutral-700 text-white font-medium text-xs leading-tight uppercase shadow-md hover:bg-sky-400 hover:shadow-lg active:bg-sky-700 active:shadow-lg transition duration-150 ease-in-out">
+                        <i class="fa-brands fa-youtube"></i>
+                    </a>
+                {:else}
+                    <!-- svelte-ignore a11y-missing-attribute -->
+                    <a rel="noreferrer" class="py-2 px-3 inline-block w-full bg-neutral-800 text-neutral-700 font-medium text-xs leading-tight uppercase shadow-md">
+                        <i class="fa-brands fa-youtube"></i>
+                    </a>
+                {/if}
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+
+</style>
--- svelte-frontend/src/lib/components/songs/SongOverview.svelte
+++ svelte-frontend/src/lib/components/songs/SongOverview.svelte
@@ -1,9 +1,37 @@
 <script lang="ts">
 
+    import * as SongAPI from "../../api/songAPI";
+    import { getRecord } from "../../api/artistAPI";
+    import SongCard from "./SongCard.svelte";
+
+    async function getData() {
+        const songs = await SongAPI.getAllSongs();
+        const artists = await Promise.all(songs.map(async (song) => {
+            return getRecord(song.artist).then((data) => data);
+        }));
+        const combinedData = songs.map((song, i) => {
+            return {
+                "song": song,
+                "artist": artists[i]
+            };
+        });
+        return combinedData;
+    }
+
 </script>
 
 <div class="h-full w-full dark:bg-neutral-700">
-
+    <div class=" grid grid-cols-7 gap-5 p-5">
+        {#await getData()}
+            <div class="absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2 ">
+                <div class="border-t-transparent border-solid animate-spin rounded-full border-sky-400 border-8 h-32 w-32"></div>
+            </div>
+        {:then data}
+            {#each data as song }
+                <SongCard artistData="{song.artist}" songData="{song.song}"/>
+            {/each}
+        {/await}
+    </div>
 </div>
 
 <style>
